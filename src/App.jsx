4794d7cb498ea4675123e0248890f549e7d279cf
Richard Lefevre <rlefevre.6505@gmail.com>
2026-01-08 10:31:04 +0000

    chore: set repo

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+//dont justcode in app.jsx, always use components
+
+//start wiht wireframe and build from a UI first approach
+//
+
+function App() {
+  //STATE
+  // variable to store api image data
+  // variable to store current image
+
+  //EFFECTS
+  // fetch api datda
+  // put fetched api data in state
+
+  //FUNCTIONS
+  // when user clicks an image - update state
+  //when user presses button that should switch image
+  //when user enters search field
+
+  return (
+    <>
+      <h1>gallery</h1>
+      <div>tuhmbnail container, images from .map</div>
+      <div>main image (or a modal)</div>
+    </>
+  );
+}
+
+export default App;
